d237fb2de5fc63a3985878812e9210edf9c90d17
Sergey Batagov <sergey030901@mail.ru>
2022-09-05 22:42:13 +0300

    task2

--- script.js
+++ script.js
@@ -1,21 +1,95 @@
-const form = document.forms.changeText
+const form = document.forms.formTask2
+
 const input = form.text
-const output = document.querySelector('.output')
-input.onchange = ()=>{
-	let val = input.value
-	let el;
-	if (/^[#].*/gm.test(val)){
-		el=document.createElement('h1')
-		el.innerHTML =`${val.slice(1)}`
-		output.append(el)
-	} else if (/^[*]{2}.*[*]{2}$/gm.test(val)){
-		el = document.createElement('b')
-		el.innerHTML = `${val.slice(2, -2)}`
-		output.append(el)
+const counter = document.getElementById('textCounter')
+
+const emailList = document.querySelector('.emailList')
+const emailListItem = document.getElementsByClassName('emailList__item')
+const emailError = document.querySelector('span.error');
+
+changeCounterText();
+buttonDelListItem();
+choiceEmail()
+
+
+form.onsubmit = (e) => {
+	e.preventDefault()
+	addEmailinEmailList()
+}
+
+input.addEventListener('input', () => {
+	if (validate()) {
+		emailError.textContent = '';
+		emailError.className = 'error';
 	} else {
-		el = document.createElement('span')
-		el.innerHTML = `${val}`
-		output.append(el)
+		emailError.textContent = 'Введите корректный email. Допустимо использовать только латинские буквы, цифры, знак подчеркивания, точку и минус.';
+		emailError.className = 'error active';
 	}
-	input.value = ''
+})
+
+function addEmailinEmailList() {
+	if (validate()){
+		li = document.createElement('li');
+		li.className = 'emailList__item';
+		li.innerHTML = `
+			<input class="emailList__radio" type="radio" id="emailChoice${emailListItem.length}" name="email" value="${input.value}">
+			<label class="emailList__label" for="emailChoice${emailListItem.length}">${input.value}</label>
+			<button type="button" class="emailList__delItem">&#9747</button>
+		`;
+	
+		emailList.append(li);
+		buttonDelListItem();
+		changeCounterText();
+		input.value = '';
+		choiceEmail();
+	} else {
+		return false;
+	}
+
+}
+
+function validate(){
+	let value = input.value;
+	let reg = /^(?<name>[\w\_\-\.]+)@(?<address>[A-Za-z]+)(?<domaine>\.[A-Za-z]{2,4})$/mi;
+	if (reg.test(value)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function buttonDelListItem() {
+	const buttonDel = document.querySelectorAll('.emailList__delItem')
+	buttonDel.forEach(e => {
+		e.addEventListener('click', () => {
+			e.parentNode.remove();
+			changeCounterText();
+			changeChoiceIndex();
+		})
+	})
+}
+
+function choiceEmail() {
+	const radioButton = document.getElementsByName('email')
+	radioButton.forEach(e => {
+		e.addEventListener('change', () => {
+			input.value = e.value;
+			emailError.textContent = '';
+			emailError.className = 'error';
+		})
+	})
+}
+
+function changeCounterText() {
+	counter.textContent = `${emailListItem.length} items`;
+}
+
+function changeChoiceIndex() {
+	const emailListItem = [...emailList.children]
+	emailListItem.forEach((e, index) => {
+		radio = e.firstElementChild;
+		label = radio.nextElementSibling;
+		radio.id = `emailChoice${index}`;
+		label.setAttribute('for', `emailChoice${index}`);
+	})
 }
